31ae040cd861d11d4ccd70e4bfc6b7b3f23c8eec
Mac <mohammed.ali.chherawalla@gmail.com>
2021-05-20 13:39:44 +0530

    add support for creating postman collection with tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import fs from 'fs'
-import {capitalize} from 'lodash'
+import {v4} from 'uuid'
+import {capitalize,cloneDeep} from 'lodash'
+import baseCollection from './base-collection.json'
+import sampleFolder from './base-folder.json'
+import sampleRequest from './base-request.json'
+
 
 const shell = require('shelljs');
 
@@ -7,25 +12,6 @@ function ucfirst(str) {
     var firstLetter = str.substr(0, 1);
     return firstLetter.toUpperCase() + str.substr(1);
 }
-
-const ENDPOINT = "https://anilist.co/graphql"
-// shell.exec(`npx cognito-cli createConfig --region=ap-southeast-1 \
-//     --userPoolWebClientId=6uobj0e81bfc2hbo1rjked632r\
-//     --userPoolId=ap-southeast-1_URpT8rFMN\
-//     --email=mac@wednesday.is --password=Wednesday1!`);
-// shell.exec(`npx cognito-cli signin > tokens.json`);
-//
-// const tokenFile = fs.readFileSync(`./tokens.json`, {encoding: 'utf-8'});
-//
-// const accessToken = JSON.parse(tokenFile).accessToken.jwtToken;
-// console.log(accessToken)
-//
-shell.exec(`rm schema.json`)
-shell.exec(`npx get-graphql-schema ${ENDPOINT} -j > schema.json`)
-// shell.exec(`npx get-graphql-schema ${ENDPOINT} -h "Authorization=${accessToken}" -j > schema.json`)
-const fileData = fs.readFileSync('schema.json', {encoding: 'utf-8'})
-const schema = JSON.parse(fileData)['__schema']
-
 
 const recursivelyHandleOfType = (arg) => {
     if (arg.type.kind === "NON_NULL" ||
@@ -134,6 +120,24 @@ function createArgsAndBody(entity, entityObj, result, variables, root, depth, va
 
 }
 
+const ENDPOINT = "https://anilist.co/graphql"
+// shell.exec(`npx cognito-cli createConfig --region=ap-southeast-1 \
+//     --userPoolWebClientId=6uobj0e81bfc2hbo1rjked632r\
+//     --userPoolId=ap-southeast-1_URpT8rFMN\
+//     --email=mac@wednesday.is --password=Wednesday1!`);
+// shell.exec(`npx cognito-cli signin > tokens.json`);
+//
+// const tokenFile = fs.readFileSync(`./tokens.json`, {encoding: 'utf-8'});
+//
+// const accessToken = JSON.parse(tokenFile).accessToken.jwtToken;
+// console.log(accessToken)
+//
+shell.exec(`rm schema.json`)
+shell.exec(`npx get-graphql-schema ${ENDPOINT} -j > schema.json`)
+// shell.exec(`npx get-graphql-schema ${ENDPOINT} -h "Authorization=${accessToken}" -j > schema.json`)
+const fileData = fs.readFileSync('schema.json', {encoding: 'utf-8'})
+const schema = JSON.parse(fileData)['__schema']
+
 
 const endpoint = ENDPOINT.replace(/(http|https):\/\//, '')
 shell.exec(`rm -rf output/${endpoint}`);
@@ -141,8 +145,19 @@ shell.exec(`rm -rf output/${endpoint}`);
 
 const queries = schema.types.find(t => t.name === "Query").fields
 const mutations = schema.types.find(t => t.name === "Mutation").fields
+
+const collection = {}
+collection.info = baseCollection.info;
+collection.info._postman_id = v4()
+collection.info.name = endpoint
+collection.item = []
 function generateOutput(list, operationName) {
-    list.forEach(e => {
+    const folder = cloneDeep(sampleFolder)
+    folder.name = operationName;
+    folder.item = []
+    folder.item.push(...list.map((e, i) => {
+        const sampleRequest = require('./base-request.json')
+        let request = cloneDeep(sampleRequest)
         const entity = recursivelyHandleOfType(e)
         if (entity.type.kind !== "SCALAR" && entity.type.kind !== "ENUM" && entity.type.kind !== "UNION") {
             const entityObj = schema.types.find(t => t.name === entity.type.name)
@@ -150,14 +165,23 @@ function generateOutput(list, operationName) {
             let [result, variables, variablesJSON] = createArgsAndBody(entity, entityObj, "", "", true, 4);
             result = `${operationName} ${result}`
             result = result.replace('()', `(${variables})`)
+            request.request.body.graphql.query = result.toString()
+            request.request.body.graphql.variables = JSON.stringify(variablesJSON)
+            request.request.url.raw = endpoint
+            request.request.url.host = [endpoint]
+            request.name = `${operationName} ${entity.type.name}`
             fs.writeFileSync(`output/${endpoint}/${operationName}/${entity.type.name}/${entity.type.name}.graphql`, result, {encoding: 'utf-8'})
             fs.writeFileSync(`output/${endpoint}/${operationName}/${entity.type.name}/variables.json`, JSON.stringify(variablesJSON), {encoding: 'utf-8'})
+            return request
         }
-    })
+    }))
+    return folder
 }
-generateOutput(queries, "query")
-generateOutput(mutations, "mutation")
+collection.item.push(generateOutput(queries, "query"))
+collection.item.push(generateOutput(mutations, "mutation"))
 
+
+fs.writeFileSync(`output/collections.json`, JSON.stringify(collection), {encoding: 'utf-8'})
 
 
 
