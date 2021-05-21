2eed84e08b21a998f212fa9ee9cf16f552b73ef9
Mac <mohammed.ali.chherawalla@gmail.com>
2021-05-21 15:03:09 +0530

    prettier

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,22 @@
-import shell from "shelljs";
-import process from "process";
-import { generateOutput } from "./index";
+#! /usr/bin/env node
+import shell from 'shelljs';
+import process from 'process';
+import { generateOutput } from './index';
 
 const args = process.argv.slice(2);
-const commandLineArgs = args.toString().split(",");
+const commandLineArgs = args.toString().split(',');
 
 if (!commandLineArgs[0]) {
   shell.exec(
     `echo Sorry! graphql-testkit requires an argument to be passed. Run graphql-testkit --help for more details`
   );
 } else {
-  if (commandLineArgs[0] === "--help") {
+  if (commandLineArgs[0] === '--help') {
     shell.echo(
-      "endpoint=GraphQL endpoint you want to create the postman collection for\n" +
-        "headers=Comma separated list of headers that you want to pass with the request to get the schema\n" +
-        "maxDepth=Maximum amount of nesting you want in the auto-generated queries and mutations\n" +
-        ""
+      'endpoint=GraphQL endpoint you want to create the postman collection for\n' +
+        'headers=Comma separated list of headers that you want to pass with the request to get the schema\n' +
+        'maxDepth=Maximum amount of nesting you want in the auto-generated queries and mutations\n' +
+        ''
     );
   } else {
     const config = createConfig(
@@ -38,9 +39,9 @@ export function createConfig(config, args) {
   const newConfig = {};
   for (let i = 0; i < args.length; i++) {
     const arg = args[i];
-    const [k, value] = arg.toString().split("=");
-    const key = k.replace("--", "");
-    if (!k.includes("--")) {
+    const [k, value] = arg.toString().split('=');
+    const key = k.replace('--', '');
+    if (!k.includes('--')) {
       return shell.echo(`Invalid arg ${key}`);
     }
     newConfig[key] = value;
