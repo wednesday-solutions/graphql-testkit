23dad71e71f5fa7e1d39fb0173724f0b3dc000e7
Mac <mohammed.ali.chherawalla@gmail.com>
2024-08-26 17:56:21 +0530

    feat: upgrade dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import globals from "globals";
+import { includeIgnoreFile } from '@eslint/compat';
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
@@ -6,35 +7,44 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
 
-export default [...compat.extends("prettier", "prettier-standard"), {
+export default [
+  ...compat.extends('prettier', 'prettier-standard'),
+  includeIgnoreFile(gitignorePath),
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.amd,
-            GLOBAL: false,
-            it: false,
-            expect: false,
-            describe: false,
-        }
+      globals: {
+        ...globals.browser,
+        ...globals.amd,
+        GLOBAL: false,
+        it: false,
+        expect: false,
+        describe: false
+      }
     },
 
     rules: {
-        "prettier/prettier": ["error", {
-            printWidth: 120,
-            tabWidth: 2,
-            useTabs: false,
-            singleQuote: true,
-            trailingComma: "none",
-            indent: 2,
-        }],
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 120,
+          tabWidth: 2,
+          useTabs: false,
+          singleQuote: true,
+          trailingComma: 'none',
+          indent: 2
+        }
+      ],
 
-        "import/no-webpack-loader-syntax": 0,
-        curly: ["error", "all"],
-    },
-}];
+      'import/no-webpack-loader-syntax': 0,
+      curly: ['error', 'all']
+    }
+  }
+];
